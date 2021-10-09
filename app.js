8292502f1699cc0ccccd1909e3688bd15f717ced
Divyangana <divyangana97@gmail.com>
2021-10-10 00:48:50 +0530

    Fixed display logic

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let outputDiv=document.querySelector("#output-username");
 var isLeap = document.querySelector("#output-right");
 var isNotLeap = document.querySelector("#output-wrong");
 var userNameTwo = document.querySelector("#op-username");
-
+var flag=0;
 
 
 btnSubmit.addEventListener("click",clickHandler);
@@ -18,42 +18,45 @@ function clickHandler(){
         let d = new Date(dateOfBirth.value);
         let year = d.getFullYear();
         console.log("The year is ",year);
-        checkLeapYear(year);
+        var bool=checkLeapYear(year,flag);
+        if(bool==1){
+            console.log("flag is 1");
+            isNotLeap.style.display="none";
+            isLeap.style.display="block";
+            outputDiv.innerText=userName.value;
+        }
+        else{
+            console.log("flag is 0");
+            isLeap.style.display="none";
+            isNotLeap.style.display="block";
+            userNameTwo.innerText=userName.value;
+        }
     }
 
 }
 
-function checkLeapYear(year){
-    if(year%4===0){
-        if(year%100){
-            if(year%400){
+function checkLeapYear(year,flag){
+    if(year%4==0){
+        if(year%100==0){
+            if(year%400==0){
                 console.log("Leap year it is");
-                isLeap.style.display="block";
-                outputDiv.innerText=userName.value;
-               
-                
+                flag=1;
             }
             else{
                 console.log("not a leap year");
-                isNotLeap.style.display="block";
-               userNameTwo.innerText=userName.value;
-                
+                flag=0;
             }
         }
         else{
             console.log("Leap year it is");
-            isLeap.style.display="block";
-            outputDiv.innerText=userName.value;
-            
+            flag=1;
         }
     }
     else{
         console.log("not a leap year");
-        isNotLeap.style.display="block";
-        userNameTwo.innerText=userName.value;
-       
+        flag=0;
     }
-
+    return flag;
 
 }
 
